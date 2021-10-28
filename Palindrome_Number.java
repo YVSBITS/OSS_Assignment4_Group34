b6804edd3766b8a07c908774d7ca2b0e2070670d
RaghuBits <91591473+2020mt93699@users.noreply.github.com>
2021-10-28 16:32:59 +0530

    Updated the Palindrome code as per review comments

--- Palindrome_Number.java
+++ Palindrome_Number.java
@@ -7,7 +7,7 @@ class PalindromeExample{
   while(n>0){    
    r=n%10;  //getting remainder *** 
    sum=(sum*10)+r;    
-   n=n*10;    
+   n=n/10;    
   }    
   if(temp==sum)    
    System.out.println("palindrome number ");    
